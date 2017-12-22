9c73414744ec0e03f2a2b85f256fb0e571d7555e
Manuel Marquez <buzondemam@gmail.com>
2017-12-21 22:22:41 -0400

    [ADD] Adds unique constraint to partners in fields name, phone and email.

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Humanytek - Manuel Marquez <manuel@humanytek.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'Partners Unique',
+    'version': '9.0.0.1.0',
+    'category': 'Hidden',
+    'author': 'Humanytek',
+    'website': "http://www.humanytek.com",
+    'license': 'AGPL-3',
+    'depends': ['base', ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False
+}
